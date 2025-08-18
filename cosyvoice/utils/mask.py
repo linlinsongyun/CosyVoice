ee4eda435badc9b3fb9ad8d353ae74e0bf79e291
linlinsongyun <37888350+linlinsongyun@users.noreply.github.com>
2025-08-18 17:10:16 +0800

    Update mask.py

--- cosyvoice/utils/mask.py
+++ cosyvoice/utils/mask.py
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import sys
 import torch
 '''
 def subsequent_mask(
@@ -86,7 +86,27 @@ def subsequent_mask(
     return mask
 
 
-def subsequent_chunk_mask_deprecated(
+def make_length_attn_mask(xs_lens):
+    """
+    xs_lens = torch.tensor([14,15,16,10,8])
+    生成 【5，16，16】的矩阵，按照lens 进行msk
+    """
+    # 获取最大长度
+    max_len = xs_lens.max()
+    
+    # 生成行索引和列索引，形状为 (1, max_len, 1) 和 (1, 1, max_len)
+    row_indices = torch.arange(max_len, device=xs_lens.device).view(1, -1, 1)
+    col_indices = torch.arange(max_len, device=xs_lens.device).view(1, 1, -1)
+    
+    # 将 xs_lens 扩展为 (bs, 1, 1)
+    xs_lens = xs_lens.view(-1, 1, 1)
+    
+    # 利用广播机制生成掩码
+    mask = (row_indices < xs_lens) & (col_indices < xs_lens)
+    
+    return mask.bool()
+
+def subsequent_chunk_mask(
         size: int,
         chunk_size: int,
         num_left_chunks: int = -1,
@@ -120,42 +140,27 @@ def subsequent_chunk_mask_deprecated(
         else:
             start = max((i // chunk_size - num_left_chunks) * chunk_size, 0)
         ending = min((i // chunk_size + 1) * chunk_size, size)
+        start = int(start)
+        ending = int(ending)
+
         ret[i, start:ending] = True
     return ret
 
 
-def subsequent_chunk_mask(
-        size: int,
-        chunk_size: int,
-        num_left_chunks: int = -1,
-        device: torch.device = torch.device("cpu"),
-) -> torch.Tensor:
-    """Create mask for subsequent steps (size, size) with chunk size,
-       this is for streaming encoder
 
-    Args:
-        size (int): size of mask
-        chunk_size (int): size of chunk
-        num_left_chunks (int): number of left chunks
-            <0: use full chunk
-            >=0: use num_left_chunks
-        device (torch.device): "cpu" or "cuda" or torch.Tensor.device
-
-    Returns:
-        torch.Tensor: mask
-
-    Examples:
-        >>> subsequent_chunk_mask(4, 2)
-        [[1, 1, 0, 0],
-         [1, 1, 0, 0],
-         [1, 1, 1, 1],
-         [1, 1, 1, 1]]
-    """
-    # NOTE this modified implementation meets onnx export requirements, but it doesn't support num_left_chunks
-    pos_idx = torch.arange(size, device=device)
-    block_value = (torch.div(pos_idx, chunk_size, rounding_mode='trunc') + 1) * chunk_size
-    ret = pos_idx.unsqueeze(0) < block_value.unsqueeze(1)
-    return ret
+def get_masks_len(mask):
+    # 确保 mask 是布尔类型（True/False）
+    if not mask.is_floating_point():
+        mask = mask.bool()
+    
+    # 展平每个样本为一维张量（形状变为 [5, 16*16]）
+    flat_mask = mask.view(mask.size(0), -1)
+    
+    # 计算每个样本中 True 的个数（形状为 [5]）
+    true_counts = flat_mask.sum(dim=1)
+    
+    # 调整为 [5, 1] 的形状
+    return true_counts.unsqueeze(1)
 
 
 def add_optional_chunk_mask(xs: torch.Tensor,
@@ -192,6 +197,123 @@ def add_optional_chunk_mask(xs: torch.Tensor,
     Returns:
         torch.Tensor: chunk mask of the input xs.
     """
+
+              
+    mask_lens = get_masks_len(masks)
+    #print('mask_lens', mask_lens)
+    mask_expand = make_length_attn_mask(mask_lens)
+
+    # Whether to use chunk mask or not
+    if use_dynamic_chunk:
+        max_len = xs.size(1)
+        if decoding_chunk_size < 0:
+            chunk_size = max_len
+            num_left_chunks = -1
+        elif decoding_chunk_size > 0:
+            chunk_size = decoding_chunk_size
+            num_left_chunks = num_decoding_left_chunks
+        else:
+            # chunk size is either [1, 25] or full context(max_len).
+            # Since we use 4 times subsampling and allow up to 1s(100 frames)
+            # delay, the maximum frame is 100 / 4 = 25.
+            #print('static_chunk_size', static_chunk_size)
+            chunk_size = static_chunk_size
+            if use_dynamic_left_chunk:
+                max_left_chunks = (max_len - 1) // chunk_size
+                #max_left_chunks = min(max_left_chunks, 10)
+                num_left_chunks = torch.randint(1, max_left_chunks, (1, )).item()
+                #print('=====num_left_chunks', num_left_chunks, 'chunk_size', chunk_size)
+            
+        chunk_masks = subsequent_chunk_mask(xs.size(1), chunk_size,
+                                            num_left_chunks,
+                                            xs.device)  # (L, L)
+        chunk_masks = chunk_masks.unsqueeze(0)  # (1, L, L)
+        chunk_masks = mask_expand & chunk_masks  # (B, L, L)
+    elif static_chunk_size > 0:
+        num_left_chunks = num_decoding_left_chunks
+        chunk_masks = subsequent_chunk_mask(xs.size(1), static_chunk_size,
+                                            num_left_chunks,
+                                            xs.device)  # (L, L)
+        chunk_masks = chunk_masks.unsqueeze(0)  # (1, L, L)
+        chunk_masks_tmp = chunk_masks
+        #print('static_chunk_size chunk_masks', chunk_masks)
+        
+        chunk_masks = mask_expand & chunk_masks  # (B, L, L)
+    else:
+        chunk_masks = masks
+    assert chunk_masks.dtype == torch.bool
+    
+
+    #torch.set_printoptions(profile="full") 
+   
+    if (chunk_masks.sum(dim=-1) == 0).sum().item() != 0:
+        # streaming模式下，chunk_masks 全部是false，
+        # chunk_mask 是尾部，length_mask 是头部，交叉为0
+        # 这种样本不要了，全部mask
+        '''
+        print('chunk_masks_tmp', chunk_masks_tmp)
+        print('masks', masks)
+        
+        
+        masks_len=torch.sum(masks, dim=2)
+        print('mask', masks_len)
+        chunk_masks_tmp_len=torch.sum(chunk_masks_tmp, dim=-1)
+        print('chunk_masks_tmp', chunk_masks_tmp_len)
+        print('chunk_masks_tmp', chunk_masks_tmp)
+
+        print('xs', xs.size())
+        print('use_dynamic_chunk', use_dynamic_chunk)
+        print('use_dynamic_left_chunk', use_dynamic_left_chunk)
+        print('decoding_chunk_size', decoding_chunk_size)
+        print('static_chunk_size', static_chunk_size)
+        print('num_decoding_left_chunks', num_decoding_left_chunks)
+        print('enable_full_context', enable_full_context)
+        '''
+        #print('get chunk_masks all false at some timestep, force set to true, make sure they are masked in futuer computation!')
+        
+        chunk_masks[chunk_masks.sum(dim=-1) == 0] = True
+        #sys.exit()
+    
+    return chunk_masks
+
+def add_optional_chunk_mask_old(xs: torch.Tensor,
+                            masks: torch.Tensor,
+                            use_dynamic_chunk: bool,
+                            use_dynamic_left_chunk: bool,
+                            decoding_chunk_size: int,
+                            static_chunk_size: int,
+                            num_decoding_left_chunks: int,
+                            enable_full_context: bool = True):
+    """ Apply optional mask for encoder.
+
+    Args:
+        xs (torch.Tensor): padded input, (B, L, D), L for max length
+        mask (torch.Tensor): mask for xs, (B, 1, L)
+        use_dynamic_chunk (bool): whether to use dynamic chunk or not
+        use_dynamic_left_chunk (bool): whether to use dynamic left chunk for
+            training.
+        decoding_chunk_size (int): decoding chunk size for dynamic chunk, it's
+            0: default for training, use random dynamic chunk.
+            <0: for decoding, use full chunk.
+            >0: for decoding, use fixed chunk size as set.
+        static_chunk_size (int): chunk size for static chunk training/decoding
+            if it's greater than 0, if use_dynamic_chunk is true,
+            this parameter will be ignored
+        num_decoding_left_chunks: number of left chunks, this is for decoding,
+            the chunk size is decoding_chunk_size.
+            >=0: use num_decoding_left_chunks
+            <0: use all left chunks
+        enable_full_context (bool):
+            True: chunk size is either [1, 25] or full context(max_len)
+            False: chunk size ~ U[1, 25]
+
+    Returns:
+        torch.Tensor: chunk mask of the input xs.
+    """
+
+              
+    
+
     # Whether to use chunk mask or not
     if use_dynamic_chunk:
         max_len = xs.size(1)
@@ -210,11 +332,13 @@ def add_optional_chunk_mask(xs: torch.Tensor,
             if chunk_size > max_len // 2 and enable_full_context:
                 chunk_size = max_len
             else:
-                chunk_size = chunk_size % 25 + 1
+                #chunk_size = chunk_size % 25 + 1
+                chunk_size = chunk_size % static_chunk_size + 1
                 if use_dynamic_left_chunk:
                     max_left_chunks = (max_len - 1) // chunk_size
                     num_left_chunks = torch.randint(0, max_left_chunks,
                                                     (1, )).item()
+                    print('=====num_left_chunks', num_left_chunks, 'chunk_size', chunk_size)
         chunk_masks = subsequent_chunk_mask(xs.size(1), chunk_size,
                                             num_left_chunks,
                                             xs.device)  # (L, L)
@@ -226,13 +350,45 @@ def add_optional_chunk_mask(xs: torch.Tensor,
                                             num_left_chunks,
                                             xs.device)  # (L, L)
         chunk_masks = chunk_masks.unsqueeze(0)  # (1, L, L)
+        chunk_masks_tmp = chunk_masks
+        #print('static_chunk_size chunk_masks', chunk_masks)
+        
         chunk_masks = masks & chunk_masks  # (B, L, L)
     else:
         chunk_masks = masks
     assert chunk_masks.dtype == torch.bool
+    
+
+    #torch.set_printoptions(profile="full") 
+   
     if (chunk_masks.sum(dim=-1) == 0).sum().item() != 0:
-        print('get chunk_masks all false at some timestep, force set to true, make sure they are masked in futuer computation!')
+        # streaming模式下，chunk_masks 全部是false，
+        # chunk_mask 是尾部，length_mask 是头部，交叉为0
+        # 这种样本不要了，全部mask
+        '''
+        print('chunk_masks_tmp', chunk_masks_tmp)
+        print('masks', masks)
+        
+        
+        masks_len=torch.sum(masks, dim=2)
+        print('mask', masks_len)
+        chunk_masks_tmp_len=torch.sum(chunk_masks_tmp, dim=-1)
+        print('chunk_masks_tmp', chunk_masks_tmp_len)
+        print('chunk_masks_tmp', chunk_masks_tmp)
+
+        print('xs', xs.size())
+        print('use_dynamic_chunk', use_dynamic_chunk)
+        print('use_dynamic_left_chunk', use_dynamic_left_chunk)
+        print('decoding_chunk_size', decoding_chunk_size)
+        print('static_chunk_size', static_chunk_size)
+        print('num_decoding_left_chunks', num_decoding_left_chunks)
+        print('enable_full_context', enable_full_context)
+        '''
+        #print('get chunk_masks all false at some timestep, force set to true, make sure they are masked in futuer computation!')
+        
         chunk_masks[chunk_masks.sum(dim=-1) == 0] = True
+        #sys.exit()
+    
     return chunk_masks
 
 
