034896d882a0b7dfef054c5b444ed29b29b1e42b
linlinsongyun <37888350+linlinsongyun@users.noreply.github.com>
2025-10-30 13:36:45 +0800

    Update prepare_data25hz.sh

--- prepare_data25hz.sh
+++ prepare_data25hz.sh
@@ -28,22 +28,22 @@ fi
 
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     for data in "${data_phn_lists[@]}"; do
-        #python mytool/extract_s3.py --filelist $data --vq-root test_data/vq_data
+        #python mytoool/extract_s3.py --filelist $data --vq-root test_data/vq_data
         data_tag=$(basename "$data" "_with_phs.txt")
-        python mytool/extract_semantic.py --filelist $data --vq-root $vq_root --emb_root $spk_root --wav-root wav_root
+        python mytoool/extract_semantic.py --filelist $data --vq-root $vq_root --emb_root $spk_root --wav-root wav_root
     done 
 fi
 
 if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
     for data in "${data_phn_lists[@]}"; do
-        #python mytool/extract_s3.py --filelist $data --vq-root test_data/vq_data
+        #python mytoool/extract_s3.py --filelist $data --vq-root test_data/vq_data
         data_tag=$(basename "$data" "_with_phs.txt")
-        python mytool/build_dataset.py --filelist $data --vq-root $vq_root --spk-root $spk_root --wav-root /mnt/nas1/ --output $protos_output/${data_tag}.proto
+        python mytoool/build_dataset.py --filelist $data --vq-root $vq_root --spk-root $spk_root --wav-root /mnt/nas1/ --output $protos_output/${data_tag}.proto
     done 
 fi
 
 if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
-    python mytool/split_pb_file.py --input-root $protos_output --output-root $split_protos_output #--split-cnt 1024
+    python mytoool/split_pb_file.py --input-root $protos_output --output-root $split_protos_output #--split-cnt 1024
 fi
 
 
